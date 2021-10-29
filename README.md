d1f357be621b6b85379cc1799d6a434c6b7e3108
brvarner <bvarner4592@gmail.com>
2021-10-29 14:35:53 -0500

    init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ExpressJS
+
+This is just a little exercises to get used to the principles behind Express.js
---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser')
+
+let app = express();
+
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.post('/contact-form', (req, res) => {
+    console.log(req.body.email);
+    console.log(req.body.name);
+    res.send('Thanks for submitting your contact info!')
+})
+
+app.use(express.static(path.join(__dirname, '../public')))
+
+
+
+app.listen(3000);
